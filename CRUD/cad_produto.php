b74bc08837f1a90db49df6601e0004c4e1cdcdf2
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-03-14 19:24:52 -0300

    Colocando a variavel e ligacao do conexao

--- CRUD/cad_produto.php
+++ CRUD/cad_produto.php
@@ -1,5 +1,10 @@
 <?php
-echo '<pre>';
-  print_r($_POST['nome_produto']);
-  echo '</pre>';
+require('../conn.php');
+
+$nome_prod = $_POST['nome_produto'];
+$qtd_produto = $_POST['qtd_produto'];
+$valor_produto = $_POST['valor_produto'];
+$categoria_produto = $_POST['categoria_produto'];
+
+
 ?>
