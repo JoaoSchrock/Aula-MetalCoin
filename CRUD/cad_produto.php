4cf1536136512de38530a4cbd283353e1fe65189
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-03-14 19:42:09 -0300

    Inserir valor das variaveis no banco de dados

--- CRUD/cad_produto.php
+++ CRUD/cad_produto.php
@@ -9,7 +9,7 @@ $categoria_produto = $_POST['categoria_produto'];
 if(empty($nome_prod) || empty($qtd_produto) ||  empty($valor_produto) ||  empty($categoria_produto)){
     echo "Voce precisa preencher todos os campos";
 } else {
-    $cad_produto = $pdo->prepare("INSERT INTO produtos (nome_produto, quantidade_produto, valor_produto, categoria_produto) VALUES (:nome_produto, :qtd_produto , :valor_produto, :categoria_produto)");
+    $cad_produto = $pdo->prepare("INSERT INTO produtos (nome_produto, quantidade_produto, valor_produto, categoria_produto) VALUES (:nome_produto, :qtd_produto , :valor_produto, :categoria_produto)") ;
 }
 
 ?>
