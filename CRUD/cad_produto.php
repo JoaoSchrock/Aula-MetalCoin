387c3df7a87327a53f2319c8f1ede9a2c4f3ad13
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-03-14 19:39:10 -0300

    Inserindo a condicao

--- CRUD/cad_produto.php
+++ CRUD/cad_produto.php
@@ -1,7 +1,7 @@
 <?php
 require('../conn.php');
 
-$nome_prod = $_POST['nome_produto'];
+$nome_produto = $_POST['nome_produto'];
 $qtd_produto = $_POST['qtd_produto'];
 $valor_produto = $_POST['valor_produto'];
 $categoria_produto = $_POST['categoria_produto'];
@@ -9,7 +9,7 @@ $categoria_produto = $_POST['categoria_produto'];
 if(empty($nome_prod) || empty($qtd_produto) ||  empty($valor_produto) ||  empty($categoria_produto)){
     echo "Voce precisa preencher todos os campos";
 } else {
-
+    $cad_produto = $pdo->prepare("INSERT INTO produtos (nome_produto, quantidade_produto, valor_produto, categoria_produto) VALUES (:nome_produto, :qtd_produto , :valor_produto, :categoria_produto)");
 }
 
 ?>
